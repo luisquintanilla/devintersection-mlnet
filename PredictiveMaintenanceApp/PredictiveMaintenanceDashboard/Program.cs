21d78550d1d264f1e29eba5ba9bf1efd846f44f3
luisquintanilla <Luis.Quintanilla@microsoft.com>
2021-12-05 18:27:58 -0500

    Add Blazor telemetry dashboard

---
+++ PredictiveMaintenanceApp/PredictiveMaintenanceDashboard/Program.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System.Text.Json;
+using Azure.Data.Tables;
+using PredictiveMaintenanceDashboard.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = Environment.GetEnvironmentVariable("DEVINT_TELEMETRY_CS");
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<TelemetryDBService>(sp =>
+{
+    return new TelemetryDBService(connectionString, "testtelemetry");
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
+
+void SeedData(string fileName)
+{
+    var rawData = File.ReadAllText(fileName);
+
+    var telemetryData = JsonSerializer.Deserialize<IEnumerable<TelemetrySeedData>>(rawData);
+
+    var client = new TableClient(connectionString, "testtelemetry");
+
+    foreach (var reading in telemetryData)
+    {
+        var telemetryEvent = new TableEntity();
+        telemetryEvent.PartitionKey = reading.Product_ID.ToString();
+        telemetryEvent.RowKey = $"{DateTimeOffset.Now.ToUnixTimeMilliseconds()}";
+        telemetryEvent["AirTemperature"] = reading.Air_temperature;
+        telemetryEvent["ProcessTemperature"] = reading.Process_temperature;
+        telemetryEvent["RotationalSpeed"] = reading.Rotational_speed;
+        telemetryEvent["Torque"] = reading.Torque;
+        telemetryEvent["ToolWear"] = reading.Tool_wear;
+        telemetryEvent["MachineFailure"] = reading.Machine_failure;
+
+        client.AddEntity(telemetryEvent);
+    }
+}
---
+++ PredictiveMaintenanceApp/PredictiveMaintenanceDashboard/Services/TelemetryDBService.cs
@@ -0,0 +1,93 @@
+﻿using System.Text.Json.Serialization;
+using Azure.Data.Tables;
+
+namespace PredictiveMaintenanceDashboard.Services
+{
+    public class TelemetryDBService
+    {
+        readonly TableClient _tableClient;
+
+        public TelemetryDBService(string connectionString, string tableName)
+        {
+            _tableClient = new TableClient(connectionString, tableName);
+        }
+
+        public TelemetryModel MapEntityToTelemetryModel(TableEntity entity)
+        {
+            var telemetryModel = new TelemetryModel
+            {
+                ProductID = entity.PartitionKey,
+                Observation = entity.RowKey,
+                Timestamp = entity.Timestamp,
+                Etag = entity.ETag.ToString(),
+            };
+
+            foreach (var key in entity.Keys)
+            {
+                telemetryModel[key] = entity[key];
+            }
+
+            return telemetryModel;
+        }
+
+        public IEnumerable<TelemetryModel> GetAllRows()
+        {
+            var entities = _tableClient.Query<TableEntity>();
+
+            return entities.Select(entity => MapEntityToTelemetryModel(entity));
+        }
+    }
+
+    public class TelemetrySeedData
+    {
+        public float UDI { get; set; }
+
+        [JsonPropertyName(@"Product ID")]
+        public string Product_ID { get; set; }
+
+        public string Type { get; set; }
+
+        [JsonPropertyName(@"Air temperature")]
+        public float Air_temperature { get; set; }
+
+        [JsonPropertyName(@"Process temperature")]
+        public float Process_temperature { get; set; }
+
+        [JsonPropertyName(@"Rotational speed")]
+        public float Rotational_speed { get; set; }
+
+        public float Torque { get; set; }
+
+        [JsonPropertyName(@"Tool wear")]
+        public float Tool_wear { get; set; }
+
+        [JsonPropertyName(@"Machine failure")]
+        public float Machine_failure { get; set; }
+    }
+
+    public class TelemetryModel
+    {
+        private Dictionary<string, object> _properties = new Dictionary<string, object>();
+
+        public DateTimeOffset? Timestamp { get; set; }
+
+        public string Etag { get; set; }
+
+        public string ProductID { get; set; }
+        public string Observation { get; set; }
+
+        public object this[string name]
+        {
+            get => (ContainsProperty(name)) ? _properties[name] : null;
+            set => _properties[name] = value;
+        }
+
+        public ICollection<string> PropertyNames => _properties.Keys;
+
+        public int PropertyCount => _properties.Count;
+
+        public bool ContainsProperty(string name) => _properties.ContainsKey(name);
+
+
+    }
+}
